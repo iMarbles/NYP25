fadf33e469851d03f3c1a5f22b5e29ec35964c85
Evelyn Tan <evelyntan@Evelyns-MacBook-Pro.local>
2017-01-15 17:13:22 +0800

    UserSocial Select & Display Image

---
+++ NYP25/UserSocialSelectImageViewController.swift
@@ -0,0 +1,45 @@
+//
+//  UserSocialSelectImageViewController.swift
+//  NYP25
+//
+//  Created by Evelyn Tan on 15/1/17.
+//  Copyright © 2017 NYP. All rights reserved.
+//
+
+import UIKit
+
+class UserSocialSelectImageViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate  {
+
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
+    
+    @IBAction func loadImageButtonTapped(sender: UIButton) {
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .photoLibrary
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imagePicker.delegate = self
+    }
+    
+    // MARK: - UIImagePickerControllerDelegate Methods
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.contentMode = .scaleAspectFit
+            imageView.image = pickedImage
+        }
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+}
