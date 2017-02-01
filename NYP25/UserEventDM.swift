de23898bcc341afbe346a34e91d232186c570428
Kenif <hi@kenif.xyz>
2017-02-01 18:06:39 +0800

    event details now working

--- NYP25/UserEventDM.swift
+++ NYP25/UserEventDM.swift
@@ -25,17 +25,23 @@ class UserEventDM: NSObject {
                 
                 let e = Event()
                 
+                e.status = r.childSnapshot(forPath: "status").value as! String
+                
+                if e.status != "O" {
+                    continue;
+                }
+                
                 e.eventId = r.key
                 e.name = r.childSnapshot(forPath: "name").value as? String
                 e.address = r.childSnapshot(forPath: "address").value as? String
                 e.imageUrl = r.childSnapshot(forPath: "image").value as? String
-                //e.badgeId = r.childSnapshot(forPath: "badge").value as? String
+                e.badgeId = r.childSnapshot(forPath: "badge").value as? String
                 e.desc = r.childSnapshot(forPath: "description").value as? String
                 e.date = r.childSnapshot(forPath: "date").value as? String
                 e.startTime = r.childSnapshot(forPath: "startTime").value as? String
-                //e.endTime = r.childSnapshot(forPath: "endTime").value as? String
-                //e.status = r.childSnapshot(forPath: "status").value as! String
-                print("Event loaded.")
+                e.endTime = r.childSnapshot(forPath: "endTime").value as? String
+                e.status = r.childSnapshot(forPath: "status").value as! String
+//                print("Event loaded.")
                 eventList.append(e)
             }
             onComplete(eventList)
